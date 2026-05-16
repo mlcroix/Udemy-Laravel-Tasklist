b078fb6209663d8631fc3e7c47707692402e0f6a
Michel du Croix <michelducroix@gmail.com>
2026-05-16 09:45:33 +0200

    Add succes message - send a success message when task has been created

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,9 @@
         @yield('title')
     </h1>
     <div>
+        @if (session('success'))
+            <p style="color: green;">{{ session('success') }}</p>
+        @endif
         @yield('content')
     </div>
 </body>
